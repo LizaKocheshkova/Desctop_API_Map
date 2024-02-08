266643d54f10f635edeaa04b541008414a79df04
LizaKocheshkova <lizakoceskova@yandex.ru>
2024-02-08 21:27:13 +0300

    Небольшие изменения в файле api

--- API/api.py
+++ API/api.py
@@ -1,42 +1,39 @@
 import requests
 
-api_server = "http://static-maps.yandex.ru/1.x/"
-api_server1 = "http://geocode-maps.yandex.ru/1.x/"
+api_static = "http://static-maps.yandex.ru/1.x/"
+api_geocode = "http://geocode-maps.yandex.ru/1.x/"
+
+
+def geocode(adress):
+    params = {
+        "apikey": "40d1649f-0493-4b70-98ba-98533de7710b",
+        "geocode": adress,
+        "format": "json"
+    }
+    response = requests.get(api_geocode, params)
+    if response:
+        json_response = response.json()
+        toponym_coodrinates = json_response["response"]["GeoObjectCollection"][
+            "featureMember"][0]["GeoObject"]["Point"]["pos"]
+        return toponym_coodrinates
 
 
 def image_map(adress=None, lon=None, lat=None):
-    if not adress:
-        delta = "0.002"
-        params = {
-            "ll": ",".join([lon, lat]),
-            "spn": ",".join([delta, delta]),
-            "l": "map"
-        }
-        response = requests.get(api_server, params=params)
-        if response:
-            map_file = "map.png"
-            with open(map_file, "wb") as file:
-                file.write(response.content)
-    else:
-        params = {
-            "apikey": "40d1649f-0493-4b70-98ba-98533de7710b",
-            "geocode": 'Москва, ул. Ак. Королева, 12',
-            "format": "json"
-        }
-        response = requests.get(api_server1, params)
-        if response:
-            json_response = response.json()
-            toponym_coodrinates = json_response["response"]["GeoObjectCollection"][
-        "featureMember"][0]["GeoObject"]["Point"]["pos"]
-            toponym_longitude, toponym_lattitude = toponym_coodrinates.split(" ")
-            delta = "0.005"
-            map_params = {
-                "ll": ",".join([toponym_longitude, toponym_lattitude]),
-                "spn": ",".join([delta, delta]),
-                "l": "map"
-            }
-            response = requests.get(api_server, map_params)
-            map_file = "map.png"
-            with open(map_file, "wb") as file:
-                file.write(response.content)
+    if adress:
+        toponym_coodrinates = geocode(adress)
+        lon, lat = toponym_coodrinates.split(" ")
+
+    delta = "0.002"
+    params = {
+        "ll": ",".join([lon, lat]),
+        "spn": ",".join([delta, delta]),
+        "l": "map"
+    }
+    response = requests.get(api_static, params=params)
+    if response:
+        map_file = "map.png"
+        with open(map_file, "wb") as file:
+            file.write(response.content)
+
+    return lon, lat, delta
 
