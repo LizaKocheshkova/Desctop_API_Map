16dc1b92f2e39797d0730217e230846a51b86693
USER <lenakarpova.05@yandex.ru>
2024-02-08 20:58:45 +0300

    добавление кнопки показать карту и создание соответствующей функции

--- main.py
+++ main.py
@@ -10,6 +10,13 @@ class Window(QMainWindow):
         super().__init__()
         uic.loadUi('win_map.ui', self)
         self.setWindowTitle('Приложение для Яндекс карт')
+        self.btn_show_map.clicked.connect(self.show_map)
+
+    def show_map(self):
+        address = self.input_address.text()
+        longitude = self.input_long.text()
+        latitude = self.input_width.text()
+        pass
 
 
 if __name__ == '__main__':
