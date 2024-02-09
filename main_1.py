138e7f03c3cd151c6bd3e3f76a99f3f4e1480cab
Dmitrii <kocheshkovds@gmail.com>
2024-02-10 00:49:01 +0300

    Заготовка на 3 задачу

---
+++ main_1.py
@@ -0,0 +1,44 @@
+from PyQt5.QtGui import QPixmap
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow, QDialog
+from PyQt5 import uic
+from PyQt5.QtCore import Qt
+from API.api import *
+
+
+class Window(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle('Приложение для Яндекс карт')
+        self.setGeometry(800, 800, 800, 800)
+        self.delta = 0.002
+        self.long = '0'
+        self.width = '0'
+
+    def keyPressEvent(self, event):
+        print(event.key(), Qt.Key_Right, Qt.Key_Left)
+        if event.key() == Qt.Key_PageUp:
+            self.delta += 0.003
+            self.show_map()
+        if event.key() == Qt.Key_PageDown:
+            if self.delta > 0.003:
+                self.delta -= 0.003
+            self.show_map()
+        if event.key() == Qt.Key_Up:
+            self.width = str(float(self.width) + self.delta)
+            self.show_map()
+        if event.key() == Qt.Key_Down:
+            self.width = str(float(self.width) - self.delta)
+            self.show_map()
+        if event.key() == Qt.Key_Right:
+            self.long = str(float(self.long) + self.delta)
+            self.show_map()
+        if event.key() == Qt.Key_Left:
+            self.long = str(float(self.long) - self.delta)
+            self.show_map()
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = Window()
+    window.show()
+    sys.exit(app.exec())
