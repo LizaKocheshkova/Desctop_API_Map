87130849ab756562f8bca5a33070c3a06e66357d
USER <lenakarpova.05@yandex.ru>
2024-02-09 18:07:10 +0300

    Изменила орфографические ошибки, поменяла главное окно, создала диалоговое окно для ввода данных, создала класс диалогового окна, добавила новые функции

--- API/api.py
+++ API/api.py
@@ -4,24 +4,24 @@ api_static = "http://static-maps.yandex.ru/1.x/"
 api_geocode = "http://geocode-maps.yandex.ru/1.x/"
 
 
-def geocode(adress):
+def geocode(address):
     params = {
         "apikey": "40d1649f-0493-4b70-98ba-98533de7710b",
-        "geocode": adress,
+        "geocode": address,
         "format": "json"
     }
     response = requests.get(api_geocode, params)
     if response:
         json_response = response.json()
-        toponym_coodrinates = json_response["response"]["GeoObjectCollection"][
+        toponym_coordinates = json_response["response"]["GeoObjectCollection"][
             "featureMember"][0]["GeoObject"]["Point"]["pos"]
-        return toponym_coodrinates
+        return toponym_coordinates
 
 
-def image_map(delta, adress=None, lon=None, lat=None, up=0, r=0):
+def image_map(delta, address=None, lon=None, lat=None, up=0, r=0):
     if adress:
-        toponym_coodrinates = geocode(adress)
-        lon, lat = toponym_coodrinates.split(" ")
+        toponym_coordinates = geocode(address)
+        lon, lat = toponym_coordinates.split(" ")
 
     # delta = "0.002"
     params = {
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from PyQt5.QtGui import QPixmap
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtWidgets import QApplication, QMainWindow, QDialog
 from PyQt5 import uic
 from PyQt5.QtCore import Qt
 from API.api import *
@@ -12,20 +12,28 @@ class Window(QMainWindow):
         uic.loadUi('win_map.ui', self)
         self.setWindowTitle('Приложение для Яндекс карт')
         self.btn_show_map.clicked.connect(self.show_map)
+        self.btn_input_data.clicked.connect(self.show_win_input_data)
         self.delta = 0.002
         self.up = 0
         self.r = 0
 
-    def show_map(self):
+    def show_map(self):  # тут нужно переделать, получить данные из диалогового окна для показа карты
         address = self.input_address.text()
-        longitude = self.input_long.text()
+        longitude = self.input_long.text()  # эти три строки скорее всего поменяются или уберутся
         latitude = self.input_width.text()
+        image_map(str(self.delta), address=address, lon=longitude, lat=latitude, up=self.up, r=self.r)
         try:
-            image_map(str(self.delta), adress=address, lon=longitude, lat=latitude, up=self.up, r=self.r)
             self.pixmap = QPixmap('map.png')
             self.label_map.setPixmap(self.pixmap)
         except Exception:
-            print('Вы не ввели данные для отображения карты')
+            pass
+
+    def show_win_input_data(self):
+        print('ok')
+        self.data = InputData()
+        self.data.show()
+        if self.data.exec_() == QDialog.Accepted:
+            self.update_welcome()
 
     def keyPressEvent(self, event):
         print(event.key(), Qt.Key_Right, Qt.Key_Left)
@@ -54,6 +62,23 @@ class Window(QMainWindow):
             print("Left")
             self.r -= self.delta
             self.show_map()
+
+
+class InputData(QDialog):
+    def __init__(self):
+        super(InputData, self).__init__()
+        uic.loadUi('input_data.ui', self)
+        self.setWindowTitle('Ввод данных')
+        self.buttonBox.accepted.connect(self.input_data_ok)
+        self.buttonBox.rejected.connect(self.log_user_not)
+
+    def input_data_ok(self):  # тут нужно доделать сохранение введенных данных для передачи их в функцию показа карты
+        address = self.input_address.text()
+        longitude = self.input_long.text()
+        latitude = self.input_width.text()
+
+    def input_data_not(self):
+        pass
 
 
 if __name__ == '__main__':
